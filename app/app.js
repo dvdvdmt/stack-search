2323c443383c1d2ec51a3f1212ec8e3e29c77155
Dmitry Davydov <dvdvdmt@gmail.com>
2016-07-31 20:13:32 +0500

    added a template to welcome screen

--- app/app.js
+++ app/app.js
@@ -7,7 +7,11 @@ angular.module('stackSearch', [
     'cQuestionInfo',
     'stackSearchServices'
 ]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
-    $routeProvider.otherwise({redirectTo: '/'});
+    $routeProvider
+        .when('/', {
+            template: '<div class="jumbotron">\n    <h1>Welcome to Stack Search</h1>\n    <p>Input some text in search form and hit Enter or click <i class="glyphicon glyphicon-search"></i></p>\n    <p><label class="btn btn-primary btn-lg" for=\'search-input\'>Lets start!</label></p>\n</div>'
+        })
+        .otherwise({redirectTo: '/'});
 }]).controller('MainCtrl', function ($scope, $location) {
     $scope.onSearch = function () {
         $location.path('/search/' + $scope.searchText)
